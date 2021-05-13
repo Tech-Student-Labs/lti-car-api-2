f3467dd2e2d34111ffea05fb8fe41b7b89252056
Robert Muehler <robertmuehler@gmail.com>
2021-05-13 16:58:07 -0400

    added test for instantiating a Vehicle

---
+++ Models/Database.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Models
+{
+
+        public class Database
+        {
+            private List<Vehicle> vehicles = new List<Vehicle>() { };
+        }
+}
---
+++ Models/Vehicle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Models
+{
+    public class Vehicle
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Year { get; set; }
+        public int Miles { get; set; }
+        public string Color { get; set; }
+        public string[] Images { get; set; }
+        public string VIN { get; set; }
+        public double sellingPrice { get; set; }
+    }
+
+
+}
