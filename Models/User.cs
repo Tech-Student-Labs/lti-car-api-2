dc1a07b0d03c284f283db411127df1eefde88390
Raaed Khan <raaedk@gmail.com>
2021-05-20 16:39:45 -0400

    created a user model

---
+++ Models/User.cs
@@ -0,0 +1,11 @@
+namespace Models
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string authorization { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string phoneNumber { get; set; }
+    }
+}
