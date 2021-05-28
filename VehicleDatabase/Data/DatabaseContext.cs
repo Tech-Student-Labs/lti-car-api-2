c64b7dd3223f5e939d033dafe8b3f22211999532
Raaed Khan <raaedk@gmail.com>
2021-05-28 14:58:02 -0400

    refactor

--- VehicleDatabase/Data/DatabaseContext.cs
+++ VehicleDatabase/Data/DatabaseContext.cs
@@ -20,7 +20,7 @@ namespace VehicleDatabase.Data
                 new Vehicle { VIN = "5Z1SL39746U411411", Make = "Honda", Model = "Civic", Year = "1997", Miles = 145000, Color = "Black", sellingPrice = 3000, ImageURI = "https://via.placeholder.com/150" },
                 new Vehicle { VIN = "7T1SL646726411440", Make = "Subaru", Model = "Impreza", Year = "2005", Miles = 175000, Color = "Blue", sellingPrice = 4000, ImageURI = "https://via.placeholder.com/150"},
                 new Vehicle { VIN = "9P1SL658486268352", Make = "Mazda", Model = "3", Year = "2007", Miles = 200000, Color = "Red", sellingPrice = 2000, ImageURI = "https://via.placeholder.com/150"},
-                new Vehicle { VIN = "YU1SL658486123463", Make = "Mitsubishi", Model = "Eclipse", Year = "2005", Miles = 75000, Color = "Purple", sellingPrice = 6000, ImageURI = "https://via.placeholder.com/150"},
+                new Vehicle { VIN = "YU1SL658486123463", Make = "Mitsubishi", Model = "Eclipse", Year = "2005", Miles = 75000, Color = "Purple", sellingPrice = 6000, ImageURI = "https://via.placeholder.com/150"}
             );
 
             base.OnModelCreating(modelBuilder);
--- VehicleDatabase/Startup.cs
+++ VehicleDatabase/Startup.cs
@@ -32,8 +32,6 @@ namespace VehicleDatabase
             {
                 c.SwaggerDoc("v1", new OpenApiInfo {Title = "VehicleDatabase", Version = "v1"});
             });
-
-            // services.AddDbContext<DatabaseContext>(options => options.use)
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
