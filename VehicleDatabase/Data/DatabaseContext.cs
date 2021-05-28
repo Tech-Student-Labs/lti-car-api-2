35060fb2350a5b7731781b55f9280e88512a4f00
Robert Muehler <robertmuehler@gmail.com>
2021-05-28 15:30:32 -0400

    fixed the seed data for vehicle to fit new model

--- VehicleDatabase/Data/DatabaseContext.cs
+++ VehicleDatabase/Data/DatabaseContext.cs
@@ -19,11 +19,31 @@ namespace VehicleDatabase.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Vehicle>().HasData(
-                new Vehicle { VIN = "4Y1SL65848Z411439", Make = "Toyota", Model = "Corolla", Year = "1997", Miles = 145000, Color = "Silver", sellingPrice = 2000, ImageURI = "https://via.placeholder.com/150" },
-                new Vehicle { VIN = "5Z1SL39746U411411", Make = "Honda", Model = "Civic", Year = "1997", Miles = 145000, Color = "Black", sellingPrice = 3000, ImageURI = "https://via.placeholder.com/150" },
-                new Vehicle { VIN = "7T1SL646726411440", Make = "Subaru", Model = "Impreza", Year = "2005", Miles = 175000, Color = "Blue", sellingPrice = 4000, ImageURI = "https://via.placeholder.com/150"},
-                new Vehicle { VIN = "9P1SL658486268352", Make = "Mazda", Model = "3", Year = "2007", Miles = 200000, Color = "Red", sellingPrice = 2000, ImageURI = "https://via.placeholder.com/150"},
-                new Vehicle { VIN = "YU1SL658486123463", Make = "Mitsubishi", Model = "Eclipse", Year = "2005", Miles = 75000, Color = "Purple", sellingPrice = 6000, ImageURI = "https://via.placeholder.com/150"}
+                new Vehicle
+                {
+                    VIN = "4Y1SL65848Z411439", Make = "Toyota", Model = "Corolla", Year = "1997", Miles = 145000,
+                    Color = "Silver", SellingPrice = 2000, Status = Vehicle.StatusCode.Inventory
+                },
+                new Vehicle
+                {
+                    VIN = "5Z1SL39746U411411", Make = "Honda", Model = "Civic", Year = "1997", Miles = 145000,
+                    Color = "Black", SellingPrice = 3000, Status = Vehicle.StatusCode.Inventory
+                },
+                new Vehicle
+                {
+                    VIN = "7T1SL646726411440", Make = "Subaru", Model = "Impreza", Year = "2005", Miles = 175000,
+                    Color = "Blue", SellingPrice = 4000, Status = Vehicle.StatusCode.Inventory
+                },
+                new Vehicle
+                {
+                    VIN = "9P1SL658486268352", Make = "Mazda", Model = "3", Year = "2007", Miles = 200000,
+                    Color = "Red", SellingPrice = 2000, Status = Vehicle.StatusCode.Pending
+                },
+                new Vehicle
+                {
+                    VIN = "YU1SL658486123463", Make = "Mitsubishi", Model = "Eclipse", Year = "2005", Miles = 75000,
+                    Color = "Purple", SellingPrice = 6000, Status = Vehicle.StatusCode.Sold
+                }
             );
 
             modelBuilder.Entity<User>().HasData(
@@ -33,10 +53,10 @@ namespace VehicleDatabase.Data
             modelBuilder.Entity<InventoryVehicle>().HasData(
                 new InventoryVehicle()
             );
-            
+
             modelBuilder.Entity<VehicleImage>().HasData(
                 new VehicleImage()
-                );
+            );
             base.OnModelCreating(modelBuilder);
         }
     }
--- VehicleDatabase/Models/History.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Models
-{
-    public class History
-    {
-        public User UserId { get; set; }
-        public Vehicle VehicleId { get; set; }
-    }
-}
