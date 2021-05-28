64784ddadab08b3208d105a5ef49a2906796349e
Raaed Khan <raaedk@gmail.com>
2021-05-28 14:57:27 -0400

    added more vehicles

--- VehicleDatabase/Data/DatabaseContext.cs
+++ VehicleDatabase/Data/DatabaseContext.cs
@@ -13,22 +13,14 @@ namespace VehicleDatabase.Data
             base.OnConfiguring(optionsBuilder);
         }
 
-        /*
-        public string VIN { get; set; }
-        public string Make { get; set; }
-        public string Model { get; set; }
-        public string Year { get; set; }
-        public int Miles { get; set; }
-        public string Color { get; set; }
-        public string[] Images { get; set; }
-        public double sellingPrice { get; set; }
-        */
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Vehicle>().HasData(
                 new Vehicle { VIN = "4Y1SL65848Z411439", Make = "Toyota", Model = "Corolla", Year = "1997", Miles = 145000, Color = "Silver", sellingPrice = 2000, ImageURI = "https://via.placeholder.com/150" },
-                new Vehicle { VIN = "5Z1SL65848A411439", Make = "Honda", Model = "Civic", Year = "1997", Miles = 145000, Color = "Black", sellingPrice = 3000, ImageURI = "https://via.placeholder.com/150" },
-                new Vehicle { VIN = "7T1SL658486411439", Make = "Subaru", Model = "Impreza", Year = "2005", Miles = 175000, Color = "Blue", sellingPrice = 4000, ImageURI = "https://via.placeholder.com/150"}
+                new Vehicle { VIN = "5Z1SL39746U411411", Make = "Honda", Model = "Civic", Year = "1997", Miles = 145000, Color = "Black", sellingPrice = 3000, ImageURI = "https://via.placeholder.com/150" },
+                new Vehicle { VIN = "7T1SL646726411440", Make = "Subaru", Model = "Impreza", Year = "2005", Miles = 175000, Color = "Blue", sellingPrice = 4000, ImageURI = "https://via.placeholder.com/150"},
+                new Vehicle { VIN = "9P1SL658486268352", Make = "Mazda", Model = "3", Year = "2007", Miles = 200000, Color = "Red", sellingPrice = 2000, ImageURI = "https://via.placeholder.com/150"},
+                new Vehicle { VIN = "YU1SL658486123463", Make = "Mitsubishi", Model = "Eclipse", Year = "2005", Miles = 75000, Color = "Purple", sellingPrice = 6000, ImageURI = "https://via.placeholder.com/150"},
             );
 
             base.OnModelCreating(modelBuilder);
