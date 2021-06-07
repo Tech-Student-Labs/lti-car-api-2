cb4e75ccd2d286253fa099c3dde1cdea71f79d4e
GannonDeWalt <gannondew@gmail.com>
2021-06-07 15:37:25 -0500

    fixed previous commit

--- VehicleWebAPI/Startup.cs
+++ VehicleWebAPI/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -33,7 +34,7 @@ namespace VehicleWebAPI
             {
                 c.SwaggerDoc("v1", new OpenApiInfo {Title = "VehicleDatabase", Version = "v1"});
             });
-            services.AddScoped<DatabaseContext>();
+            services.AddDbContext<DatabaseContext>(options => options.UseSqlServer("Server=localhost,1433;Database=Vehicle;User=sa;Password=SusPassword2!"));
             services.AddScoped<IVehicleDatabaseService, VehicleDatabaseService>();
         }
 
