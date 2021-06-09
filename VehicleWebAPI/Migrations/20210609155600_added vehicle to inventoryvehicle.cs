71b9f4c972c7db739d1685854682655878971fdb
rmuehler <67pha0vz!!!!!>
2021-06-09 14:38:49 -0400

    new migration for added reference to vehicle in vehicleimages

---
+++ VehicleWebAPI/Migrations/20210609155600_added vehicle to inventoryvehicle.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VehicleWebAPI.Migrations
+{
+    public partial class addedvehicletoinventoryvehicle : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Inventory_VehicleId",
+                table: "Inventory",
+                column: "VehicleId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Inventory_Vehicles_VehicleId",
+                table: "Inventory",
+                column: "VehicleId",
+                principalTable: "Vehicles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Inventory_Vehicles_VehicleId",
+                table: "Inventory");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Inventory_VehicleId",
+                table: "Inventory");
+        }
+    }
+}
---
+++ VehicleWebAPI/Migrations/20210609174119_added vehicle reference to vehicleimages model.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VehicleWebAPI.Migrations
+{
+    public partial class addedvehiclereferencetovehicleimagesmodel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
