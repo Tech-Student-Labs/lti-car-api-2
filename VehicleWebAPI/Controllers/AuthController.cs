3cd32df689974c418feb4c0948ea9f72708382dc
Andrew Mitschke <ammitchky@gmail.com>
2021-06-10 10:30:32 -0500

    added libraries to AuthController

--- VehicleWebAPI/Controllers/AuthController.cs
+++ VehicleWebAPI/Controllers/AuthController.cs
@@ -1,3 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
 [Route("api/auth")]
 [ApiController]
 public class AuthController : ControllerBase
