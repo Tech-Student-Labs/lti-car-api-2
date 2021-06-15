9c73d1f73add09c8f62ab4af8f86d73f7f0f0e0b
Okinz <steven14812@gmail.com>
2021-06-15 09:51:49 -0400

    Seeded more vehicle data and inventory

--- VehicleWebAPI/Migrations/20210614184119_Added more seed vehicles to database.Designer.cs
+++ VehicleWebAPI/Migrations/20210614184119_Added more seed vehicles to database.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using VehicleDatabase.Data;
 
 namespace VehicleWebAPI.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20210614184119_Added more seed vehicles to database")]
+    partial class Addedmoreseedvehiclestodatabase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
