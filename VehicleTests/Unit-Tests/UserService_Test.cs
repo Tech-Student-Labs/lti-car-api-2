04ea07989c4917ac8b8cf1101930874cdb4f1f10
Raaed Khan <raaedk@gmail.com>
2021-06-15 13:16:44 -0400

    AddUser Unit Test

--- VehicleTests/Unit-Tests/UserService_Test.cs
+++ VehicleTests/Unit-Tests/UserService_Test.cs
@@ -12,9 +12,9 @@ namespace VehicleTests.Unit_Tests
     public class UserService_Test
     {
         [Fact]
-        public void GetInventoryVehiclesMethod_ShouldCallDBContextMethod_ToGetFirstAllVehicles()
+        public void AddingAUser_ShouldCallAddUserFunctionInService()
         {
-        //Given the service has been injected with the database context
+            //Given the service has been injected with the database context
             var userSet = new List<User> {};
 
             var user = new User
@@ -27,25 +27,25 @@ namespace VehicleTests.Unit_Tests
                     UserName = "testperson",
                     Password = "testpass",
                 };
-        var queryableUserSet = userSet.AsQueryable();
+            var queryableUserSet = userSet.AsQueryable();
 
-        Mock<DbSet<User>> MockSet = new Mock<DbSet<User>>();
-        MockSet.As<IQueryable<User>>().Setup(m => m.Provider).Returns(queryableUserSet.Provider);
-        MockSet.As<IQueryable<User>>().Setup(m => m.Expression).Callback(() => {}).Returns(queryableUserSet.Expression);
-        MockSet.As<IQueryable<User>>().Setup(m => m.ElementType).Returns(queryableUserSet.ElementType);
-        MockSet.As<IQueryable<User>>().Setup(m => m.GetEnumerator()).Returns(queryableUserSet.GetEnumerator);
-        Mock<DatabaseContext> MockContext = new Mock<DatabaseContext>();
-        MockContext.Setup(p => p.Users).Returns(MockSet.Object);
-        MockContext.Setup(p => p.Add(It.IsAny<User>())).Callback((User user) => userSet.Add(user));
-        UserDatabaseService UserService = new UserDatabaseService(MockContext.Object);
+            Mock<DbSet<User>> MockSet = new Mock<DbSet<User>>();
+            MockSet.As<IQueryable<User>>().Setup(m => m.Provider).Returns(queryableUserSet.Provider);
+            MockSet.As<IQueryable<User>>().Setup(m => m.Expression).Callback(() => {}).Returns(queryableUserSet.Expression);
+            MockSet.As<IQueryable<User>>().Setup(m => m.ElementType).Returns(queryableUserSet.ElementType);
+            MockSet.As<IQueryable<User>>().Setup(m => m.GetEnumerator()).Returns(queryableUserSet.GetEnumerator);
+            Mock<DatabaseContext> MockContext = new Mock<DatabaseContext>();
+            MockContext.Setup(p => p.Users).Returns(MockSet.Object);
+            MockContext.Setup(p => p.Add(It.IsAny<User>())).Callback((User user) => userSet.Add(user));
+            UserDatabaseService UserService = new UserDatabaseService(MockContext.Object);
 
 
-        //When GetAllVehicles is called
-        UserService.AddUser(user);
+            //When GetAllVehicles is called
+            UserService.AddUser(user);
         
-        //Then the database context method to grab the vehicles
-        //should be called
-        MockContext.Verify(db => db.Users.Add(user), Times.Once());
+            //Then the database context method to grab the vehicles
+            //should be called
+            MockContext.Verify(db => db.Users.Add(user), Times.Once());
         }
     }
 }
