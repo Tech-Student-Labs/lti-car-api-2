12edf45e63a2adcf34fa6379c370a50e492fff9e
Andrew Mitschke <ammitchky@gmail.com>
2021-06-16 09:14:57 -0500

    git

--- VehicleTests/Unit-Tests/VehicleService_Test.cs
+++ VehicleTests/Unit-Tests/VehicleService_Test.cs
@@ -54,5 +54,52 @@ namespace VehicleTests.Unit_Tests
         //should be called
         MockContext.Verify(db => db.Vehicles, Times.Once());
         }
+
+        [Fact]
+        public void GetVehiclesByUsernameMethod_ShouldCallDBContextMethod_ToGetListOfUsersVehicles()
+        {
+        //Given the service has been injected with the database context
+        var vehicles = new List<Vehicle> {
+            new Vehicle
+                {
+                    Id = 1,
+                    VIN = "4Y1SL65848Z411439", Make = "Toyota", Model = "Corolla", Year = 1997, Miles = 145000,
+                    Color = "Silver", SellingPrice = 2000, Status = Vehicle.StatusCode.Inventory, UserId = 1,
+                    User = new User{UserName = "testname"}
+                },
+            new Vehicle
+                {
+                    Id = 2,
+                    VIN = "5Z1SL39746U411411", Make = "Honda", Model = "Civic", Year = 1997, Miles = 145000,
+                    Color = "Black", SellingPrice = 3000, Status = Vehicle.StatusCode.Inventory, UserId = 1,
+                    User = new User{UserName = "testname"}
+                },
+            new Vehicle
+                {
+                    Id = 3,
+                    VIN = "7T1SL646726411440", Make = "Subaru", Model = "Impreza", Year = 2005, Miles = 175000,
+                    Color = "Blue", SellingPrice = 4000, Status = Vehicle.StatusCode.Inventory, UserId = 2,
+                    User = new User{UserName = "testname2"}
+                }
+        };
+        var queryableVehicles = vehicles.AsQueryable();
+
+        Mock<DbSet<Vehicle>> MockSet = new Mock<DbSet<Vehicle>>();
+        MockSet.As<IQueryable<Vehicle>>().Setup(m => m.Provider).Returns(queryableVehicles.Provider);
+        MockSet.As<IQueryable<Vehicle>>().Setup(m => m.Expression).Callback(() => {}).Returns(queryableVehicles.Expression);
+        MockSet.As<IQueryable<Vehicle>>().Setup(m => m.ElementType).Returns(queryableVehicles.ElementType);
+        MockSet.As<IQueryable<Vehicle>>().Setup(m => m.GetEnumerator()).Returns(queryableVehicles.GetEnumerator);
+        Mock<DatabaseContext> MockContext = new Mock<DatabaseContext>();
+        MockContext.Setup(p => p.Vehicles).Returns(MockSet.Object);
+        VehicleDatabaseService VehicleService = new VehicleDatabaseService(MockContext.Object);
+
+
+        //When GetAllVehicles is called
+        VehicleService.GetVehicleByUsername("testname");
+        
+        //Then the database context method to grab the vehicles
+        //should be called
+        MockContext.Verify(db => db.Vehicles, Times.Once());
+        }
     }
 }
