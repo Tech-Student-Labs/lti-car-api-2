bee3427a9f7f66572e634df96e73597947d28fa1
GannonDeWalt <gannondew@gmail.com>
2021-06-18 08:54:43 -0500

    started getbyidtest

--- VehicleTests/E2E-Tests/User/User_GET_Route.cs
+++ VehicleTests/E2E-Tests/User/User_GET_Route.cs
@@ -1,8 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Reflection;
+using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 using FluentAssertions;
@@ -12,6 +14,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using VehicleDatabase.Data;
 using VehicleWebAPI;
+using VehicleWebAPI.Models;
 using Xunit;
 
 namespace VehicleTests.E2E_Tests
@@ -30,6 +33,27 @@ namespace VehicleTests.E2E_Tests
                     services.AddDbContext<DatabaseContext>(options => options.UseInMemoryDatabase("user" + guid));
                 });
         }
+        private class TokenHolder{
+            public string Token { get; set; }
+        }
+        public async Task<string> AppendJWTHeader(DatabaseContext db, HttpClient client, string username = "johndoe") {
+            db.Users.Add(new User{Email = "johndoe@test.com", UserName = username, Password = "def@123"});
+            db.SaveChanges();
+
+            //handle login for JWT Token
+            var user = new User{Email = "johndoe2@test.com", UserName = username, Password = "def@123"};
+            StringContent query = new StringContent(JsonSerializer.Serialize(user), Encoding.UTF8, "application/json");
+
+            var loginResponse = await client.PostAsync("/api/auth/login", query);
+            var content = await loginResponse.Content.ReadAsStringAsync();
+
+            var result = JsonSerializer.Deserialize<TokenHolder>(content,
+                new JsonSerializerOptions {PropertyNamingPolicy = JsonNamingPolicy.CamelCase});
+            
+            client.DefaultRequestHeaders.Add("Authorization", "Bearer " + result.Token);
+
+            return result.Token;
+        }
          
         [Fact]
         public async Task Should_ReturnAnEmpyList_WhenUserDBIsEmpty()
@@ -45,6 +69,31 @@ namespace VehicleTests.E2E_Tests
 
             //Then return an empty list
             result.Count.Should().Be(0);
+        }
+
+        [Fact]
+        public async Task TestName()
+        {
+        //Given that the service is running
+        var testServer = new TestServer(hostBuilder);
+        var client = testServer.CreateClient();
+        var db = testServer.Services.GetRequiredService<DatabaseContext>();
+        db.Database.EnsureDeleted();
+
+        var user = new User{
+            UserName = "johndoe",
+            Email = "test@test.com",
+            Id = 2,
+        };
+
+        await db.Users.AddAsync(user);
+
+        db.SaveChanges();
+        await AppendJWTHeader(db, client, "johndoe");
+        //When GET is called with a paramater of an ID
+        var response = await client.GetAsync("/User/Profile");
+        //Then return a 200 code
+        response.StatusCode.Should().Be(200);
         }
     }
 }
